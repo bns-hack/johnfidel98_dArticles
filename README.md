d108964e289dea01f32f64803df7b47fa6a0c5ef
John Nanda <johnfidel98@gmail.com>
2023-07-06 19:47:25 +0300

    complated basic design

---
+++ README.md
@@ -0,0 +1,3 @@
+# myWebsite
+
+Personal website for developers to showcase their projects and skills (developed with Material Kit 2 PRO by Creative Tim)
---
+++ src/App.js
@@ -0,0 +1,51 @@
+/**
+=========================================================
+* Material Kit 2 PRO React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-pro-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useEffect } from "react";
+import Web3 from 'web3';
+
+// react-router components
+import { Routes, Route, useLocation } from "react-router-dom";
+
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+// Material Kit 2 PRO React themes
+import theme from "assets/theme";
+
+// Material Kit 2 PRO React routes
+import Home from "home";
+
+import { abi, address } from './config';
+
+export default function App() {
+  const { pathname } = useLocation();
+
+  // Setting page scroll to 0 when changing the route
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </ThemeProvider>
+  );
+}
