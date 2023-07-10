111629ad05bd537596108f6f08f8df44e4105f47
John Nanda <johnfidel98@gmail.com>
2023-07-10 13:35:13 +0300

    changed contract

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 import articleContracts from "./ArticlesContract.json";
 
-export const contractAddress = "0x2F788f79B090C3dA17A6c4404353eA2b46018A76";
+export const contractAddress = "0xc9db00d7cb05980807A119EAd2603346B2Cab112";
 export const myAddress = "0x483e682441C0e97fAa259CD9dA4C7D332f1A443C";
 export const contractABI = articleContracts.abi;
 
